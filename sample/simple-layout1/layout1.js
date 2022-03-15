54f603f88fa9a73b4e24fd3f257a1dc3e3c1f1eb
Anran0716 <anranzheng0716@163.com>
2022-03-15 19:04:35 -0400

    data collection & basic visualization

--- sample/simple-layout1/layout1.js
+++
@@ -1,5 +0,0 @@
-let map = L.map('map').setView([30, -90], 12);
-
-L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
-}).addTo(map);
