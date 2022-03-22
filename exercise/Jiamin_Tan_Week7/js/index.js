98a0add67d5b9aef40d23d12a4de4c92fcf3cf9c
tan_jiamin@yahoo.com <39011116+jiamintan@users.noreply.github.com>
2022-03-21 23:16:52 -0400

    reset View in map

--- exercise/Jiamin_Tan_Week7/js/index.js
+++ exercise/Jiamin_Tan_Week7/js/index.js
@@ -1,6 +1,8 @@
 /* globals showdown */
 
-let map = L.map('map').setView([40.00, -75.14], 12);
+let map = L.map('map').setView([0, 0], 0);
+let layerGroup = L.layerGroup().addTo(map);
+let lifeCollection = { features: [] };
 
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
