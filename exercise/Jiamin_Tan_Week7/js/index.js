12fe5ce66c260acb0c196c18aec928d41ecd9fcc
tan_jiamin@yahoo.com <39011116+jiamintan@users.noreply.github.com>
2022-03-21 23:18:13 -0400

    added Mjumbe's example

--- exercise/Jiamin_Tan_Week7/js/index.js
+++ exercise/Jiamin_Tan_Week7/js/index.js
@@ -27,3 +27,114 @@ fetch('https://opendata.arcgis.com/datasets/8bc0786524a4486bb3cf0f9862ad0fbf_0.g
       },
     }).addTo(map);
   });
+
+/////////////////////////////////////////////////////////////
+//copied from Mjumbe
+
+let currentSlideIndex = 0;
+
+const slideTitleDiv = document.querySelector('.slide-title');
+const slideContentDiv = document.querySelector('.slide-content');
+const slidePrevButton = document.querySelector('#prev-slide');
+const slideNextButton = document.querySelector('#next-slide');
+const slideJumpSelect = document.querySelector('#jump-to-slide');
+
+function updateMap(collection) {
+  layerGroup.clearLayers();
+  const geoJsonLayer = L.geoJSON(collection, { pointToLayer: (p, latlng) => L.marker(latlng) })
+    .bindTooltip(l => l.feature.properties.label)
+    .addTo(layerGroup);
+
+  return geoJsonLayer;
+}
+
+function makeEraCollection(era) {
+  return {
+    type: 'FeatureCollection',
+    features: lifeCollection.features.filter(f => f.properties.era === era),
+  };
+}
+
+function showSlide(slide) {
+  const converter = new showdown.Converter({ smartIndentationFix: true });
+
+  slideTitleDiv.innerHTML = `<h2>${slide.title}</h2>`;
+  slideContentDiv.innerHTML = converter.makeHtml(slide.content);
+
+  const collection = slide.era ? makeEraCollection(slide.era) : lifeCollection;
+  const layer = updateMap(collection);
+
+  function handleFlyEnd() {
+    if (slide.showpopups) {
+      layer.eachLayer(l => {
+        l.bindTooltip(l.feature.properties.label, { permanent: true });
+        l.openTooltip();
+      });
+    }
+    map.removeEventListener('moveend', handleFlyEnd);
+  }
+
+  map.addEventListener('moveend', handleFlyEnd);
+  if (slide.bounds) {
+    map.flyToBounds(slide.bounds);
+  } else if (slide.era) {
+    map.flyToBounds(layer.getBounds());
+  }
+}
+
+function showCurrentSlide() {
+  const slide = slides[currentSlideIndex];
+  showSlide(slide);
+}
+
+function goNextSlide() {
+  currentSlideIndex++;
+
+  if (currentSlideIndex === slides.length) {
+    currentSlideIndex = 0;
+  }
+
+  showCurrentSlide();
+}
+
+function goPrevSlide() {
+  currentSlideIndex--;
+
+  if (currentSlideIndex < 0) {
+    currentSlideIndex = slides.length - 1;
+  }
+
+  showCurrentSlide();
+}
+
+function jumpToSlide() {
+  currentSlideIndex = parseInt(slideJumpSelect.value, 10);
+  showCurrentSlide();
+}
+
+function initSlideSelect() {
+  slideJumpSelect.innerHTML = '';
+  for (const [index, slide] of slides.entries()) {
+    const option = document.createElement('option');
+    option.value = index;
+    option.innerHTML = slide.title;
+    slideJumpSelect.appendChild(option);
+  }
+}
+
+function loadLifeData() {
+  fetch('data/journey.json')
+    .then(resp => resp.json())
+    .then(data => {
+      lifeCollection = data;
+      showCurrentSlide();
+    });
+}
+
+slidePrevButton.addEventListener('click', goPrevSlide);
+slideNextButton.addEventListener('click', goNextSlide);
+slideJumpSelect.addEventListener('click', jumpToSlide);
+
+initSlideSelect();
+showCurrentSlide();
+loadLifeData();
