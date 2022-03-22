a19d4390d132cb9692198f17d9fc33c7fb23cdb5
tan_jiamin@yahoo.com <39011116+jiamintan@users.noreply.github.com>
2022-03-21 22:27:43 -0400

    filtered census tracts

--- exercise/Jiamin_Tan_Week7/js/index.js
+++ exercise/Jiamin_Tan_Week7/js/index.js
@@ -1,7 +1,27 @@
 /* globals showdown */
 
-let map = L.map('map').setView([0, 0], 0);
+let map = L.map('map').setView([40.00, -75.14], 12);
 
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
 	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 }).addTo(map);
+
+fetch('https://opendata.arcgis.com/datasets/8bc0786524a4486bb3cf0f9862ad0fbf_0.geojson')
+  .then(resp => resp.json())
+  .then(data => {
+    L.geoJSON(data, {
+      filter(feature) {
+        if (feature.properties.NAME10 == 13 ||
+          feature.properties.NAME10 == 20 ||
+          feature.properties.NAME10 == 32 ||
+          feature.properties.NAME10 == 33 ||
+          feature.properties.NAME10 == 36)
+          return true;
+      },
+      onEachFeature(feature, layer) {
+        layer.bindTooltip(feature.properties.NAMELSAD10);
+        layer.setStyle({ fillColor: "#000000" });
+        layer.setStyle({ color: "#4287f5" });
+      },
+    }).addTo(map);
+  });
