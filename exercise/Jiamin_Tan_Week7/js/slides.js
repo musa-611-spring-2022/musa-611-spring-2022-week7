2a8acd059e9b9074cd6e01b402e6ce60e76272b8
tan_jiamin@yahoo.com <39011116+jiamintan@users.noreply.github.com>
2022-03-24 22:32:02 -0400

    added all slides

--- exercise/Jiamin_Tan_Week7/js/slides.js
+++ exercise/Jiamin_Tan_Week7/js/slides.js
@@ -1,6 +1,6 @@
 const slides = [
   {
-    title: "Introduction Slide",
+    title: "Introduction",
     content: `
       Introduction of the site...
     `,
@@ -9,11 +9,83 @@ const slides = [
   },
 
   {
-    title: 'Amenities Slide',
+    title: 'Existing: Site Overview',
+    content: `
+      There is one Indego Bikeshare Station.
+    `,
+    section: 'SiteOverview',
+    showpopups: true,
+  },
+
+  {
+    title: 'Existing: Amenities',
     content: `
       Some amenities nearby.
     `,
     section: 'Amenities',
+    showpopups: true,
+  },
+
+  {
+    title: 'Existing: Transit',
+    content: `
+      Three bus routes passing the site.
+    `,
+    section: 'Transit',
+    showpopups: true,
+  },
+
+  {
+    title: 'Plan: Affordable Housing',
+    content: `
+      There is one Indego Bikeshare Station.
+    `,
+    section: 'AffordableHousing',
+    showpopups: true,
+  },
+
+  {
+    title: 'Plan: Community Center',
+    content: `
+      There is one Indego Bikeshare Station.
+    `,
+    section: 'CommunityCenter',
+    showpopups: true,
+  },
+
+  {
+    title: 'Plan: Small Retail',
+    content: `
+      There is one Indego Bikeshare Station.
+    `,
+    section: 'SmallRetail',
+    showpopups: true,
+  },
+
+  {
+    title: 'Plan: Green Space',
+    content: `
+      There is one Indego Bikeshare Station.
+    `,
+    section: 'GreenSpace',
+    showpopups: true,
+  },
+
+  {
+    title: 'Plan: Parking',
+    content: `
+      There is one Indego Bikeshare Station.
+    `,
+    section: 'Parking',
+    showpopups: true,
+  },
+
+  {
+    title: 'Conclusion',
+    content: `
+      There is one Indego Bikeshare Station.
+    `,
+    section: 'Conclusion',
     showpopups: true,
   }
 ];
