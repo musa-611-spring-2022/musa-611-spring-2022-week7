794f526671b349b8333ff8e355557227d56bdd2b
aidanpcole <90789077+aidanpcole@users.noreply.github.com>
2022-03-28 09:42:28 -0400

    adding rough midterm

--- exercise/template/js/slides.js
+++ exercise/template/js/slides.js
@@ -1,11 +1,249 @@
-const slide1 =
+//const slide1 =
+
+//const slides = [
+//  {
+//    title: "This is my slide title",
+//    content: `
+//      Here is some sample content for this slide.
+//    `,
+//    bounds: [[-75, -180], [75, 180]],
+//  },
+//];
+
 
 const slides = [
   {
-    title: "This is my slide title",
+    title: "Top Fifteen Most Important Battles of The U.S. Revolutionary War",
     content: `
-      Here is some sample content for this slide.
+      Here we present a snapshot of the 15 most important battles of the U.S.
+      Revolutionary War told geospatially. All coordinate data and battle summary
+      information derived from https://latitude.to/.
     `,
+    name: null,
     bounds: [[-75, -180], [75, 180]],
+  },
+
+  {
+    title: 'Battles of Lexington and Concord',
+    content: `
+      The Battles of Lexington and Concord (April 19, 1775) were the first
+      military engagements of the American Revolutionary War. They were fought
+      on April 19, 1775, in Middlesex County, Province of Massachusetts Bay,
+      within the towns of Lexington, Concord, Lincoln, Menotomy
+      (present-day Arlington), and Cambridge, near Boston. The result was an
+      American victory.
+    `,
+    name: 'First',
+    showpopups: true
+  },
+
+  {
+    title: 'Battle of Bunker Hill',
+    content: `
+      The Battle of Bunker Hill was fought on June 17, 1775, during the Siege of
+      Boston in the early stages of the American Revolutionary War. Even though
+      they won the battle and were able to push the Patriots away from the crucial
+      Charleston peninsula, the British experienced far more casualties.
+    `,
+    name: 'Second',
+    showpopups: true
+  },
+
+  {
+    title: 'Battle of Quebec',
+    content: `
+      The Battle of Quebec (French: Bataille de Québec) was fought on December
+      31, 1775, between American Continental Army forces and the British
+      defenders of Quebec City early in the American Revolutionary War. The
+      battle was the first major defeat of the war for the Americans, and it
+      came with heavy losses. General Richard Montgomery was killed, Benedict
+      Arnold was wounded, and Daniel Morgan and more than 400 men were taken
+      prisoner.
+    `,
+    name: 'Third',
+    showpopups: true
+  },
+
+  {
+    title: 'Siege of Boston',
+    content: `
+      The Siege of Boston (April 19, 1775 – March 17, 1776) concluded the opening
+      phase of the American Revolutionary War. New England militiamen prevented
+      the movement by land of the British Army garrisoned in what was then the
+      peninsular town of Boston, Massachusetts. Both sides had to deal with
+      resource supply and personnel issues over the course of the siege.
+      British resupply and reinforcement activities were limited to sea access.
+      On March 17th 1776 (11 months after the seige began), George Washington
+      and the Continental Army seized Dorchester Heights, just outside of
+      Boston, and pointed their cannons at the British ships anchored in the
+      harbor, leading to a British retreat.
+    `,
+    name: 'Fourth',
+    showpopups: true
+  },
+
+  {
+    title: 'Battle of Fort Sullivan',
+    content: `
+      The Battle of Fort Sullivan (June 28, 1776), often disregarded as a minor
+      action during the American Revolutionary War, was a British attempt to
+      expand the war southwards. Just near Charleston, Fort Sullivan provided a
+      major seaport. When the British failed to capture it, it forced the war's
+      focus on the north for the next couple of years.
+    `,
+    name: 'Fifth',
+    showpopups: true
+  },
+
+  {
+    title: 'Battle of Long Island',
+    content: `
+      The Battle of Long Island, also known as the Battle of Brooklyn or the
+      Battle of Brooklyn Heights, fought on August 27, 1776, was the largest
+      battle of the war. The Patriots were outflanked and routed on Long
+      Island, but were able to escape to Manhattan. A major defeat for
+      the Continental Army under General George Washington and the beginning of
+      a successful campaign that gave the British control of the strategically
+      important city of New York.
+    `,
+    name: 'Sixth',
+    showpopups: true
+  },
+
+  {
+    title: 'Battle of Fort Washington',
+    content: `
+      The Battle of Fort Washington was fought in New York on November 16, 1776
+      during the American Revolutionary War between the United States and Great
+      Britain. It was a decisive British victory that gained the surrender of
+      the remnent of the garrison of Fort Washington near the north end of
+      Manhattan Island. Perhaps the worst Patriot defeat during the war, this
+      battle led to the British capture of over 3000 colonists in Manhattan.
+    `,
+    name: 'Seventh',
+    showpopups: true
+  },
+
+  {
+    title: 'Battle of Trenton',
+    content: `
+      The Battle of Trenton was a small but pivotal battle during the American
+      Revolutionary War which took place on the morning of December 26, 1776,
+      in Trenton, New Jersey. After General George Washington's crossing of the
+      Delaware River north of Trenton the previous night, Washington led the
+      main body of the Continental Army against Hessian soldiers garrisoned at
+      Trenton. After a brief battle, nearly the entire Hessian force was
+      captured, with negligible losses to the Americans.
+    `,
+    name: 'Eighth',
+    showpopups: true
+  },
+
+  {
+    title: 'Battle of Freeman Farm',
+    content: `
+      The Battle of Freeman Farm (September 19, 1777) was the First Battle of
+      Saratoga and marked one of the longest land battles in the Revolutionary
+      War. British General John Burgoyne led the large invasion army up the
+      Champlain Valley from Canada, hoping to meet a similar force marching
+      northward from New York City. However, the southern force never arrived,
+      and Burgoyne was surrounded by American forces in upstate New York.
+      Although the result was a tactical British victory, as they held the field of
+      battle at the end of the day, it set the British up for failure in the
+      second battle.
+    `,
+    name: 'Nineth',
+    showpopups: true
+  },
+
+  {
+    title: 'Battles of Bemis Heights',
+    content: `
+      The Battle of Bemis Heights (October 7, 1777), otherwise known as the
+      Second Battle of Saratoga, marked the climax of the Saratoga campaign
+      giving a decisive victory to the Americans over the British in the
+      American Revolutionary War. Fought after the Battle of Freeman Farm
+      and on the same stretch of land, the battle resulted in the surrender of
+      over 6000 British troops, shocking the world and ultimately inducing France
+      to partake in the war.
+    `,
+    name: 'Tenth',
+    showpopups: true
+  },
+
+  {
+    title: 'Battle of Newport',
+    content: `
+      The Battle of Newport (August 29, 1778), or The Battle of Rhode Island,
+      was a failed American campaign, often overlooked as insignificant, that
+      not only stopped American military momentum gained from Saratoga and the
+      recovery of Philadelphia, but showed that the alliance with France would
+      not bring a speedy end to the war. The northern theater remained in a
+      stalemate for the rest of the war.
+    `,
+    name: 'Eleventh',
+    showpopups: true
+  },
+
+  {
+    title: 'Siege of Charleston',
+    content: `
+      The Siege of Charleston (March 29 - May 12, 1780) was another attempt by
+      the British to shift the focus of the war onto the South. After suffering
+      significant loses in their northern strategy in 1777 and after losing
+      control of Philadelphia in 1778, the British moved South hoping to claim
+      victory there. Commanding the Charleston garrison was Major General
+      Benjamin Lincoln who surrendered his forces to the British after the six
+      week siege. This is largely considered as one of the greatest American
+      defeats in the war.
+    `,
+    name: 'Twelfth',
+    showpopups: true
+  },
+
+  {
+    title: 'Battle of Kings Mountain',
+    content: `
+      The Battle of Kings Mountain (October 7, 1780) was a decisive victory in
+      South Carolina for the Patriot militia over the Loyalist militia in the
+      Southern campaign of the American Revolutionary War. This led to the
+      British having to rethink their strategy in the South and demonstrated
+      that their subsidiary forces could easily be defeated.
+    `,
+    name: 'Thirteenth',
+    showpopups: true
+  },
+
+  {
+    title: 'Battle of Cowpens',
+    content: `
+      The Battle of Cowpens (January 17, 1781) was a decisive victory by the
+      Continental Army forces under Brigadier General Daniel Morgan in the
+      Southern campaign of the American Revolutionary War over the British Army
+      led by Colonel Banastre Tarleton. It was a turning point in the reconquest
+      of South Carolina from the British, as it renewed the spirits of the
+      American troops and put to a hault to British offensive momentum in the
+      South.
+    `,
+    name: 'Fourteenth',
+    showpopups: true
+  },
+
+  {
+    title: 'Siege of Yorktown and American Victory',
+    content: `
+      The Siege of Yorktown, Battle of Yorktown, Surrender at Yorktown or German
+      Battle, ending on October 19, 1781 at Yorktown, Virginia, was a decisive
+      victory by a combined force of American Continental Army troops led by
+      General George Washington and French Army troops led by the Comte de
+      Rochambeau over a British Army commanded by British lord and Lieutenant
+      General Charles Cornwallis. The culmination of the Yorktown campaign, the
+      siege proved to be the last major land battle of the American
+      Revolutionary War in the North American theater, as the surrender by
+      Cornwallis, and the capture of both him and his army, prompted the British
+      government to negotiate an end to the conflict.
+    `,
+    name: 'Fifteenth',
+    showpopups: true
   },
 ];
